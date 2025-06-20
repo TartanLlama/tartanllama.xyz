82a99716b48d41544399d5afbcda74ae4501045c
Sy Brand <tartanllama@gmail.com>
2025-06-20 12:12:12 +0100

    Draft of wasm plugin post

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import mdx from '@astrojs/mdx';
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import { SITE } from "./src/config";
@@ -8,7 +9,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [sitemap()],
+  integrations: [sitemap(),mdx()],
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: {
